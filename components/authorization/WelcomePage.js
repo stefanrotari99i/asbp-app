47cda68e3d6afbd2e5560069ba5e4e892d9e007e
Stefan <stefan.rotari99i@gmail.com>
2022-05-12 17:49:54 +0300

    v0.1 Add navigation

--- components/authorization/WelcomePage.js
+++ components/authorization/WelcomePage.js
@@ -1,3 +1,13 @@
+import { SafeAreaView } from "react-native"
+import { View } from "react-native-web"
 
 
-export c
+export const WelcomePage = () => {
+    return(
+        <SafeAreaView>
+            <View>
+                
+            </View>
+        </SafeAreaView>
+    )
+}
